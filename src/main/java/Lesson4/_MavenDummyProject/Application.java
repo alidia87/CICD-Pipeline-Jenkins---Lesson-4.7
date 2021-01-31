65b68de2a6ba6669998ff07603a62ab7efd7fd66
student <alidia87@gmail.com>
2021-01-31 15:04:12 +0000

    Created Dummy Maven Project

---
+++ src/main/java/Lesson4/_MavenDummyProject/Application.java
@@ -0,0 +1,13 @@
+package Lesson4._MavenDummyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/Lesson4/_MavenDummyProject/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Lesson4._MavenDummyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
